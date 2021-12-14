8ef0d1626fb331e1af9fea840104f088afcb71d5
codrin.vasiliu <codrin.vasiliu@gammbit.ro>
2021-12-14 22:55:50 +0200

    Added exceptions to be used in main

---
+++ src/exceptions.py
@@ -0,0 +1,14 @@
+class InvalidArgumentCount(Exception):
+    pass
+
+
+class InvalidFolderPath(Exception):
+    pass
+
+
+class InvalidFilePath(Exception):
+    pass
+
+
+class InvalidJsonFile(Exception):
+    pass
